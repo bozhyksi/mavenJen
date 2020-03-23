0258987365aa886cc13e65ec90329f16a649e334
bozhyksi <bozhyksi1988>
2020-03-23 16:41:11 +0200

    hj

--- src/test/java/testA.java
+++ src/test/java/testA.java
@@ -1,12 +1,15 @@
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class testA {
     @Test
     public void ttt(){
         open("https://www.google.com/");
-        Assert.assertTrue(false);
+        $(By.name("q")).should(Condition.exist);
     }
 }
