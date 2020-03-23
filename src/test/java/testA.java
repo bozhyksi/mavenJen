70f2aef936c2eba103e27f13d48d26d1cd6fdf02
bozhyksi <bozhyksi1988>
2020-03-23 16:39:07 +0200

    h

--- src/test/java/testA.java
+++ src/test/java/testA.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -6,5 +7,6 @@ public class testA {
     @Test
     public void ttt(){
         open("https://www.google.com/");
+        Assert.assertTrue(false);
     }
 }
